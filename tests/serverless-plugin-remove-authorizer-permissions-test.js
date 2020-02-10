f28c36e6ac2bf3664892fbbe378da197ff439494
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2020-02-10 19:42:45 -0300

    Plugin implementation

---
+++ tests/serverless-plugin-remove-authorizer-permissions-test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const assert = require('assert');
+
+const cloneDeep = require('lodash.clonedeep');
+
+const ServerlessPluginRemoveAuthorizerPermissions = require('../');
+
+describe('ServerlessPluginRemoveAuthorizerPermissions', () => {
+
+	const serverless = {
+		cli: {
+			log: () => {}
+		},
+		service: {
+			provider: {
+				compiledCloudFormationTemplate: {
+					Resources: {
+						SomeResource: {},
+						SomeOtherResource: {},
+						RandomLambdaPermissionApiGateway: {}
+					}
+				}
+			}
+		}
+	};
+
+	it('Should set the proper hooks', () => {
+
+		const serverlessPluginRemoveAuthorizerPermissions = new ServerlessPluginRemoveAuthorizerPermissions(serverless);
+		const hooks = Object.keys(serverlessPluginRemoveAuthorizerPermissions.hooks);
+
+		assert.deepStrictEqual(hooks, ['after:aws:package:finalize:mergeCustomProviderResources']);
+	});
+
+	it('Should not remove anything if no permissions are matched', () => {
+
+		const testConfig = cloneDeep(serverless);
+
+		const serverlessPluginRemoveAuthorizerPermissions = new ServerlessPluginRemoveAuthorizerPermissions(testConfig);
+		serverlessPluginRemoveAuthorizerPermissions.removePermissions();
+
+		assert.deepStrictEqual(testConfig, serverless);
+	});
+
+	it('Should remove any resource that matches the authorizers permissions', () => {
+
+		const testConfig = cloneDeep(serverless);
+		testConfig.service.provider.compiledCloudFormationTemplate.Resources.MyAuthorizerLambdaPermissionApiGateway = {};
+		testConfig.service.provider.compiledCloudFormationTemplate.Resources.SomeOtherAuthorizerLambdaPermissionApiGateway = {};
+
+		const serverlessPluginRemoveAuthorizerPermissions = new ServerlessPluginRemoveAuthorizerPermissions(testConfig);
+		serverlessPluginRemoveAuthorizerPermissions.removePermissions();
+
+		assert.deepStrictEqual(testConfig, serverless);
+	});
+
+});
